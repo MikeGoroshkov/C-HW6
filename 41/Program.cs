356533fafa846fa4833b71a62e837bddafde687d
Mikhail Goroshkov <gormike86@gmail.com>
2022-10-19 13:03:56 +0300

    OK

---
+++ 41/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int[] arr = InputArray(5);
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine(CountPositiveNums(arr));
+
+int[] InputArray(int countNumbers)
+{
+    int[] array = new int[countNumbers];
+    for (int i = 0; i < countNumbers; i++)
+    {
+        Console.WriteLine($"Введите число №{i}: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+int CountPositiveNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count += 1;
+    }
+    return count;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
---
+++ 43/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine($"Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine($"Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double[] coordinates = LineCrossCoordinates(k1, k2, b2, b1);
+Console.WriteLine();
+Console.WriteLine($"Координаты пересечения прямых: ({coordinates[0]}, {coordinates[1]})");
+
+double[] LineCrossCoordinates(double k1, double k2, double b2, double b1)
+{
+    double[] coor = new double[2];
+    coor[0] = (b2 - b1) / (k1 - k2);
+    coor[1] = (b1 * k2 - b2 * k1) / (k2 - k1);
+    return coor;
+}
